64c4daf3e39410e82e16c21b8dc4c4cca3c7b328
Drake-Lundstrom <drakelundstrom@gmail.com>
2024-05-02 07:57:38 -0400

    updated front end app

--- frontEnd/my-react-app/src/App.tsx
+++ frontEnd/my-react-app/src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import styled from "styled-components";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <StyledButton onClick={() => setCount(count + 1)}>Click me</StyledButton>
+      <h1></h1>
+      <StyledImage
+        src={"https://stirtrek.com/icons/opengraph.jpg"}
+        alt="React Logo"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+const StyledImage = styled.img`
+  max-height: 60vh;
+  max-width: 90%;
+`;
+
+const StyledButton = styled.button`
+  background-color: #fca311;
+  color: #ffffff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+`;
